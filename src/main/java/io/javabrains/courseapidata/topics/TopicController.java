5310a45b3d3d909bdc1b903a7525c7ddf9d803a1
smadjapa <smadjapa@gmail.com>
2023-05-19 15:27:06 +0300

    Inital Commit

---
+++ src/main/java/io/javabrains/courseapidata/topics/TopicController.java
@@ -0,0 +1,39 @@
+package io.javabrains.courseapidata.topics;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicsService topicsService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return topicsService.getTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicsService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicsService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicsService.updateTopic(topic, id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicsService.deleteTopic(id);
+    }
+
+}
